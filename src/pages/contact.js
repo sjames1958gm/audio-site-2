1e3146a5f870f50b75c90bd58785faeae3c83092
Stephen James <sajames1958@gmail.com>
2021-01-31 18:06:17 -0600

    add form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,28 +2,46 @@ import React from "react"
 import { graphql } from "gatsby"
 
 import SEO from "../components/seo"
-import { Heading, Text } from "@chakra-ui/react"
+import { Heading } from "@chakra-ui/react"
+import { FormiumForm, defaultComponents } from "@formium/react"
+
+const TextInput = props => (
+  <input style={{ border: "1px red #ddd" }} {...props} />
+)
+const formiumComponents = {
+  ...defaultComponents,
+  TextInput,
+}
 
 export const contactQuery = graphql`
-query ContactsQuery {
-    allFormiumForm {
-      edges {
-        node {
-          id
-          slug
-        }
-      }
+  query ContactsQuery {
+    formiumForm(slug: { eq: "contactform" }) {
+      id
+      name
+      slug
+      projectId
+      schema
+      updateAt
     }
   }
 `
 
 const ContactPage = ({ data }) => {
   console.log(data)
+  const formiumFormData = data.formiumForm
+
   return (
     <>
       <SEO title="About" />
       <Heading>Contact AudioCORE</Heading>
-      <Text>AudioCORE - the first shareable headphone company</Text>
+      <FormiumForm
+        data={formiumFormData}
+        components={formiumComponents}
+        onSubmit={async values => {
+          //   await formium.submitForm(values)
+          //   alert("Success!")
+        }}
+      />
     </>
   )
 }
